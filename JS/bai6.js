77f4cab91e89a582c81e337043efbe0f6a966a88
thuan2207 <vuonghutyhuan1@.com>
2024-08-08 12:58:56 +0700

    hello

--- JS/bai6.js
+++ JS/bai6.js
@@ -1,15 +1,3 @@
-
-        let array=[10,-3,5,-11,-20,16,35,18,-7,9,2];
-        let array_sort=[];
-        while (array.length!=0) {
-            let min=Math.min(...array);
-            for (let i = 0; i < array.length; i++) {
-                if (array[i]==min) {
-                    array_sort.push(array[i]);
-                    array.splice(i,1);
-                };                
-            }
-        };
-        console.log(array_sort);
-        console.log(array);
-        
+let numbers = [1,5,3,1,332,-1,-122,-4,-31,22];
+        console.log("Mảng ban đầu: ", numbers);
+        console.log("Mảng sau khi được sắp xếp: ", numbers.sort((a, b) => a - b)); 
